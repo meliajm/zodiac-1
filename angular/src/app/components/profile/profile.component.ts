fd74660199cf0c961aca69396aec4f93a7a4fcd6
Kendra Walton <mswalton15@gmail.com>
2020-09-17 22:05:32 -0400

    style added

--- angular/src/app/components/profile/profile.component.ts
+++ angular/src/app/components/profile/profile.component.ts
@@ -12,35 +12,5 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // function signs() {
-  //   date=document.zodiac.date.value;
-  //   month=document.zodiac.month.selectedIndex;
-    
-  //   with (document.zodiac.sign){
-    
-  //   if (month == 1 && date >=20 || month == 2 && date <=18) {value = “Aquarius”;}
-  //   if (month == 1 && date > 31) {value = “Huh?”;}
-  //   if (month == 2 && date >=19 || month == 3 && date <=20) {value = “Pisces”;}
-  //   if (month == 2 && date > 29) {value = “Say what?”;}
-  //   if (month == 3 && date >=21 || month == 4 && date <=19) {value = “Aries”;}
-  //   if (month == 3 && date > 31) {value = “OK.  Whatever.”;}
-  //   if (month == 4 && date >=20 || month == 5 && date <=20) {value = “Taurus”;}
-  //   if (month == 4 && date > 30) {value = “I’m soooo sorry!”;}
-  //   if (month == 5 && date >=21 || month == 6 && date <=21) {value = “Gemini”;}
-  //   if (month == 5 && date > 31) {value = “Umm … no.”;}
-  //   if (month == 6 && date >=22 || month == 7 && date <=22) {value = “Cancer”;}
-  //   if (month == 6 && date > 30) {value = “Sorry.”;}
-  //   if (month == 7 && date >=23 || month == 8 && date <=22) {value = “Leo”;}
-  //   if (month == 7 && date > 31) {value = “Excuse me?”;}
-  //   if (month == 8 && date >=23 || month == 9 && date <=22) {value = “Virgo”;}
-  //   if (month == 8 && date > 31) {value = “Yeah. Right.”;}
-  //   if (month == 9 && date >=23 || month == 10 && date <=22) {value = “Libra”;}
-  //   if (month == 9 && date > 30) {value = “Try Again.”;}
-  //   if (month == 10 && date >=23 || month == 11 && date <=21) {value = “Scorpio”;}
-  //   if (month == 10 && date > 31) {value = “Forget it!”;}
-  //   if (month == 11 && date >=22 || month == 12 && date <=21) {value = “Sagittarius”;}
-  //   if (month == 11 && date > 30) {value = “Invalid Date”;}
-  //   if (month == 12 && date >=22 || month == 1 && date <=19) {value = “Capricorn”;}
-  //   if (month == 12 && date > 31) {value = “No way!”;}
-  //   }
+  
 }
--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -11,27 +11,22 @@ export class UserService {
 
   private base_url:string;
   
-
-<<<<<<< HEAD
-  constructor(private http:HttpClient) {this.base_url = "http://localhost:8080/zodiac/";}
-=======
   constructor(private http:HttpClient) {
     this.base_url = "http://localhost:8080/Zodiac/";
   }
->>>>>>> 789113519976bf1bc247681e5429663e2d5c1ebc
 
   getUser(): Observable<User[]>{
 
-    // const headerDict = {
-    //   'Content-Type': 'application/json',
-    //   'Accept': 'application/json',
-    //   'Access-Control-Allow-Headers': 'Content-Type',
-    //   'Access-Control-Allow-Origin': '*',
-    // }
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+    }
     
-    // const requestOptions = {               
-    //   headers: new HttpHeaders(headerDict), 
-    // };
+    const requestOptions = {               
+      headers: new HttpHeaders(headerDict), 
+    };
     
     return this.http.get(`${this.base_url}user`) as Observable<User[]>;
   };
